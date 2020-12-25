ef719fdc775a7f0616147aea1bf7f3d122b6c40b
Cougarpatt <kunlapath.paengsa@gmail.com>
2020-12-25 23:16:20 +0700

    Interview Test

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace alpha
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, j, n;
+            Console.Write("Enter number : ");
+            n = int.Parse(Console.ReadLine());
+            for (i = 0; i < n; i++)
+            {
+                for (j = 0; j < n; j++)
+                {
+                if(j==i||j==n-1-i)
+                        Console.Write("*");
+                    else 
+                        Console.Write(" ");}
+                Console.WriteLine();
+            }
+        }
+    }
+}
